b4f748f050b2847fe5f521fa55ca4b7119531277
dyegospeciais <dyegospeciais@gmail.com>
2016-09-23 17:21:51 -0300

    Create Cliente.java

---
+++ ManeiraCorreta/Cliente.java
@@ -0,0 +1,14 @@
+package GoodPracticesExample;
+
+import java.util.List;
+
+/**
+ * Created by dyegospeciais on 22/09/2016.
+ */
+public class Client {
+    public static void main(String[] args){
+        Inventory inventory = new Inventory();
+        List<String> items = inventory.getStock();
+        System.out.println(items.size());
+    }
+}
