05136ca04eda385b8ffaf8a769c9f006fa25ca4e
Gabriela Aimée <gabrielaimeeg@hotmail.com>
2016-09-23 19:15:09 -0300

    Exemplo de boas práticas com funções pequenas.

---
+++ BoasPraticas/Java/FuncoesPequenas.java
@@ -0,0 +1,11 @@
+public static class Calculadora{
+
+	public int soma(int primeiroNumero, int segundoNumero){
+	return 	primeiroNumero + segundoNumero;
+	}
+
+	public int subtrai(int primeiroNumero, int segundoNumero){
+	return 	primeiroNumero - segundoNumero;
+	}
+
+}
